0a27712c46b02baee7a2b1943c722f32acb2a85d
gwongilnam <nimpa3201@naver.com>
2025-07-16 02:19:13 +0900

    feat: 타입 매개변수를 활용한 AnimalHospitalV2 제네릭 클래스 구현 및 활용 예제 추가
    - AnimalHospitalV2<T> 클래스에서 set, checkUp, bigger 메서드 정의
    - checkUp()에서는 Object 메서드만 사용 가능함을 실습
    - bigger() 메서드는 타입 미지정으로 인해 비교 기능 구현 불가함을 주석 처리
    - AnimalHospitalMainV2에서 Dog, Cat, Integer 등 다양한 타입으로 실습
    - 제네릭의 한계 및 타입 안정성 문제에 대한 예제 구성

---
+++ src/generic/test/ex3/AnimalHospitalV2.java
@@ -0,0 +1,31 @@
+ package generic.test.ex3;
+
+
+ import generic.animal.Animal;
+
+ public class AnimalHospitalV2 <T> {
+
+    private T animal;
+
+    public void set(T animal){
+        this.animal = animal;
+    }
+
+    public void checkUp() {
+        animal.toString();
+        animal.equals(null);
+
+        // T의 타입을 메서드를 정의하는 시점에는 알 수 없다. Object의 기능만 가능
+//        System.out.println("동물의 이름 " + animal.getName());
+//        System.out.println("동물의 크기 " + animal.getSize());
+//        animal.sound();
+    }
+
+    public Animal bigger(T target) {
+        // 컴파일 오류
+        //tSize() > target.getSize() ? animal : target; // 둘 중 큰 개를 반환
+        return  null;
+    }
+
+
+}
