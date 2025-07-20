d65a1c3771d86421c1adc61e44d6998318e75def
gwongilnam <nimpa3201@naver.com>
2025-07-20 16:48:16 +0900

    feat: 제네릭과 와일드카드 차이 비교 예제 코드 추가
    - Box<T>와 Box<?>를 활용하여 제네릭 메서드와 와일드카드 메서드 차이 구현
    - printGenericV1~V2, printWildcardV1~V2, printAndReturn 메서드 작성
    - Dog, Cat, Object 타입의 박스를 활용해 다양한 호출 방식 테스트
    - 와일드카드는 타입 변경 불가, 제네릭 메서드는 타입 인자로 타입 결정 가능함을 비교
    
    메서드의 타입들을 특정 시점에 변경하려면 제네릭 타입이나 제네릭 메서드를 사용해야 한다.
    와일드카드는 이미 만들어진 제네릭 타입을 전달받아서 활용할 때 사용된다.
    즉, 메서드의 타입을 동적으로 제어하려면 <T>를 사용하는 것이 적합하며,
    단순 활용만 필요한 경우에는 와일드카드를 사용하는 것이 좋다.

---
+++ src/generic/test/ex5/WildCardEx.java
@@ -0,0 +1,46 @@
+package generic.test.ex5;
+
+import generic.animal.Animal;
+
+public class WildCardEx {
+
+    static <T> void printGenericV1(Box<T> box){
+        System.out.println(" T = " + box.get());
+    }
+
+
+    //Box<Dog>, Box<Cat>, Box<Object>
+    static void printWildcardV1(Box<?> box){
+        System.out.println("? = " + box.get());
+    }
+
+    static <T extends Animal> void printGenericV2(Box<T> box){
+        T t = box.get();
+        System.out.println("이름 = " + t.getName());
+    }
+
+    static void printWildcardV2(Box< ? extends Animal> box){
+        Animal animal = box.get();
+        System.out.println("이름 = " + animal.getName());
+    }
+
+    static <T extends  Animal> T printAndReturnGeneric(Box<T> box) {
+        T t = box.get();
+        System.out.println("이름 = " + t.getName());
+        return t;
+    }
+
+    static  Animal printAndReturnWildCard(Box<? extends Animal> box) {
+        Animal animal = box.get();
+        System.out.println("이름 = " + animal.getName());
+        return animal;
+    }
+
+    /**
+     * 메서드의 타입들을 특정 시점에 변경하려면 제네릭 타입이나, 제네릭 메서드를 사용해야 한다.
+     * 와일드카드는 이미 만들어진 제네릭 타입을 전달 받아서 활용할 때 사용한다. 따라서 메서드의 타입들을 타입 인자를 통
+     * 해 변경할 수 없다. 쉽게 이야기해서 일반적인 메서드에 사용한다고 생각하면 된다.
+     * 정리하면 제네릭 타입이나 제네릭 메서드가 꼭 필요한 상황이면 `<T>` 를 사용하고, 그렇지 않은 상황이면 와일드카드를
+     * 사용하는 것을 권장한다.
+     */
+}
