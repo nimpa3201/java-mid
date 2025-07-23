c1ad69e93b7430d5d974eda16c2f3a9f0fb60c80
gwongilnam <nimpa3201@naver.com>
2025-07-24 08:17:20 +0900

    feat: MyLinkedListV2 삽입 및 삭제 기능 추가
    - add(index, element) 메서드 구현: 원하는 위치에 노드 삽입 가능
      - index == 0일 경우 맨 앞에 삽입 (O(1))
      - 그 외에는 해당 위치 이전 노드를 찾아 삽입 (O(N))
    - remove(index) 메서드 구현: 지정 위치 노드 삭제
      - 첫 번째 노드 삭제 시 first 포인터 갱신
      - 중간/마지막 노드는 이전 노드를 찾아 연결 조정
    - Node 내부 상태 초기화 (item, next = null)로 GC 유도

---
+++ src/collection/link/MyLinkedListV2Main.java
@@ -0,0 +1,42 @@
+package collection.link;
+
+
+public class MyLinkedListV2Main {
+    public static void main(String[] args) {
+        MyLinkedListV2 list = new MyLinkedListV2();
+
+        // 마지막에 추가 O(N)
+        System.out.println("=== 마지막에 추가 ===");
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+
+        // 첫 번째 항목에 추가 , 삭제
+        System.out.println("=== 첫 번째 항목에 추가 ===");
+        list.add(0,"d"); // O(1)
+        System.out.println("list = " + list);
+
+        System.out.println("=== 첫 번째 항목에 삭제 ===");
+        list.remove(0);
+        System.out.println("list = " + list);
+
+        // 중간 항목에 추가, 삭제
+        System.out.println("=== 두번째 항목에 추가 ===");
+        list.add(1,"e"); // O(N)
+        System.out.println("list = " + list);
+
+
+        System.out.println("=== 두번째 항목에 삭제 ===");
+        list.remove(1); // remove O(N)
+        System.out.println("list = " + list);
+
+
+
+
+
+
+
+
+    }
+}
