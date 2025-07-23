9acdea3f79262c0d9802f9d37a92187d1914ee9f
gwongilnam <nimpa3201@naver.com>
2025-07-23 19:44:41 +0900

    feat: Node toString() 커스텀 구현으로 연결 리스트 형태 출력 (A->B->C)

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -5,8 +5,39 @@ public class Node {
     Object item;
     Node next;
 
-    public Node(Object item){
+    public Node(Object item) {
         this.item = item;
     }
 
+/*
+  // IDE 생성 toString()
+  @Override
+    public String toString() {
+        return "Node{" +
+            "item=" + item +
+            ", next=" + next +
+            '}';
+    }*/
+
+
+    // A -> B-> C
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this; //  x01
+        sb.append("[");
+
+        while (x != null) {
+            sb.append(x.item);
+
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
 }
