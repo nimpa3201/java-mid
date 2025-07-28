039a6f87ed8b2575d9d86643fe3ee69cea6fe1e1
gwongilnam <nimpa3201@naver.com>
2025-07-28 22:33:56 +0900

    feat: 리스트 추상화1 - 인터페이스 도입

---
+++ src/collection/list/MyArrayList.java
@@ -0,0 +1,111 @@
+package collection.list;
+
+import java.util.Arrays;
+
+public class MyArrayList<E> implements MyList<E> {
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+
+
+
+    @Override
+    public void add(int index, E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        shiftRightFrom(index);
+        elementData[index] = e;
+        size++;
+
+    }
+
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i - 1];
+        }
+    }
+
+
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = 2 * oldCapacity;
+        // 배열을 새로 만들고 기존 배열을 새로운 배열에 복사
+       /*
+       Object[] newArr = new Object[newCapacity];
+
+        for (int i = 0; i < elementData.length; i++) {
+            newArr[i] = elementData[i];
+        }
+        */
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+
+    @Override
+    public E get(int index) {
+        return (E) elementData[index];
+    }
+    @Override
+    public E set(int index, Object element) {
+        E oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    @Override
+    public E remove(int index){
+        E oldValue = get(index);
+        // 데이터 이동
+        shiftLeftFrom(index);
+
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+
+    // 코드 추가 요소의 index부터 마지막까지 왼쪽으로 밀기
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size-1; i++) {
+            elementData[i] = elementData[i+1];
+        }
+    }
+    @Override
+    public int indexOf(E o) {
+        for (int i = 0; i < size; i++) {
+            if (elementData[i].equals(o)) {
+                return i;
+            }
+            ;
+        }
+        return -1;
+    }
+
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size)) +
+            " size= " + size + " , capacity= " + elementData.length;
+    }
+}
