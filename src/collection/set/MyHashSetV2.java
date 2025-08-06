77765acd7b44b179a8d38642e6af6d36bf97662e
gwongilnam <nimpa3201@naver.com>
2025-08-06 13:23:34 +0900

    feat: MyHashSetV2를 Object 기반 범용 Set 구조로 변경
    - Integer 전용 구현에서 Object 수용 구조로 수정
    - 다양한 타입의 값 저장 가능하도록 범용성 개선
    - 단, 제네릭 타입(<T>)을 사용한 구조는 아님

---
+++ src/collection/set/MyHashSetV2.java
@@ -0,0 +1,78 @@
+package collection.set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MyHashSetV2 {
+
+    static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    private LinkedList<Object>[] buckets;
+
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    public MyHashSetV2() {
+        initBuckets();
+    }
+
+    public MyHashSetV2(int capacity) {
+        this.capacity = capacity;
+        initBuckets();
+    }
+
+    private void initBuckets() {
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public boolean add(Object value) {
+
+        int index = hashIndex(value);
+        LinkedList<Object> bucket = buckets[index];
+        if (bucket.contains(value)) {
+            return false;
+        }
+        bucket.add(value);
+        size++;
+        return true;
+
+    }
+
+    public boolean contains(Object searchValue){
+        int index = hashIndex(searchValue);
+        return buckets[index].contains(searchValue);
+    }
+
+    public boolean remove(Object value){
+        int index = hashIndex(value);
+        if (buckets[index].contains(value)){
+            buckets[index].remove(value);
+            size--;
+            return true;
+        }
+        return false;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV2{" +
+            "buckets=" + Arrays.toString(buckets) +
+            ", size=" + size +
+            ", capacity=" + capacity +
+            '}';
+    }
+
+    private int hashIndex(Object value) {
+        int hashCode = value.hashCode();
+        return Math.abs(hashCode %capacity);
+    }
+
+
+}
